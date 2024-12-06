d5463756c8935b9cee2002a05eb1367a52a1dc48
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-12-06 15:25:40 +0100

    directions with diagonals

---
+++ src/Aoc/DirectionDiag.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      :  Aoc.Direction
+-- Description :  Directions on a 2D grid
+-- Copyright   :  2023 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Sun Dec 10 20:37:01 2023.
+module Aoc.DirectionDiag
+  ( Direction (..),
+  )
+where
+
+data Direction = N | NE | E | SE | S | SW | W | NW
+  deriving (Show, Eq, Ord, Bounded, Enum)
