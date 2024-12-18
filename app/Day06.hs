bdec58e15ed5c233ea0b94ade0a0163fb2ed8cf4
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-12-18 21:53:08 +0100

    day 06: parsing

--- app/Day06.hs
+++ app/Day06.hs
@@ -4,10 +4,30 @@ module Main
 where
 
 import Aoc
+import Aoc.Array (pMatrix)
 import Data.Attoparsec.Text (Parser)
+import Data.Massiv.Array (Array, B (..), Ix2, computeAs, findIndex, map)
+import Data.Maybe (fromJust)
+import Prelude hiding (map)
 
-pInput :: Parser Text
-pInput = undefined
+data Field = G | O
+  deriving (Show)
+
+fromChar :: Char -> Field
+fromChar '.' = G
+fromChar '#' = O
+fromChar x = error $ "invalid char: " <> [x]
+
+type Pitch = Array B Ix2 Field
+
+extractPlayer :: Array B Ix2 Char -> (Ix2, Pitch)
+extractPlayer xs = (p, computeAs B $ map toField xs)
+  where
+    p = fromJust $ findIndex (== '^') xs
+    toField c = if c == '^' then G else fromChar c
+
+pInput :: Parser (Ix2, Pitch)
+pInput = extractPlayer <$> pMatrix id
 
 main :: IO ()
 main = do
