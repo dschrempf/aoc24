dfd1e22e9f6458118279029a938c0c4fc95d6473
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-12-18 22:31:56 +0100

    day 06: part 1

--- app/Day06.hs
+++ app/Day06.hs
@@ -5,9 +5,12 @@ where
 
 import Aoc
 import Aoc.Array (pMatrix)
+import Aoc.Direction (Direction (..), moveNStepsInDirection, turnRight)
 import Data.Attoparsec.Text (Parser)
-import Data.Massiv.Array (Array, B (..), Ix2, computeAs, findIndex, map)
+import Data.Massiv.Array (Array, B (..), Ix2, computeAs, findIndex, map, (!?))
 import Data.Maybe (fromJust)
+import Data.Set (Set, empty, insert)
+import qualified Data.Set as S
 import Prelude hiding (map)
 
 data Field = G | O
@@ -29,7 +32,28 @@ extractPlayer xs = (p, computeAs B $ map toField xs)
 pInput :: Parser (Ix2, Pitch)
 pInput = extractPlayer <$> pMatrix id
 
+data State = State
+  { _pos :: !Ix2,
+    _dir :: !Direction,
+    visited :: !(Set Ix2)
+  }
+
+move :: Pitch -> State -> Maybe State
+move xs (State p d vs) = case xs !? p' of
+  Nothing -> Nothing
+  Just G -> Just $ State p' d $ insert p' vs
+  Just O -> Just $ State p (turnRight d) vs
+  where
+    p' = moveNStepsInDirection 1 p d
+
+loopUntil :: (a -> Maybe a) -> a -> a
+loopUntil f x = case f x of
+  Nothing -> x
+  Just x' -> loopUntil f x'
+
 main :: IO ()
 main = do
-  d <- parseChallengeT (Sample 6 1) pInput
-  print d
+  (start, pitch) <- parseChallengeT (Full 6) pInput
+  let si = State start N empty
+      se = loopUntil (move pitch) si
+  print $ S.size $ visited se
