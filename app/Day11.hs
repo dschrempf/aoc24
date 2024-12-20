d6e36b378255b884c75d68e74e0feed930f85e74
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-12-20 14:44:16 +0100

    day 11: part 1

--- app/Day11.hs
+++ app/Day11.hs
@@ -4,12 +4,31 @@ module Main
 where
 
 import Aoc
-import Data.Attoparsec.Text (Parser)
+import Aoc.Function (nTimesStrict)
+import Aoc.Parse (skipHorizontalSpace)
+import Data.Attoparsec.Text (Parser, decimal, sepBy1')
 
-pInput :: Parser Text
-pInput = undefined
+type Stone = Int
+
+type Stones = [Int]
+
+-- >>> blink 2024
+-- [20,24]
+
+blink :: Stone -> [Stone]
+blink 0 = [1]
+blink x
+  | even nDigits = map read [take n2 xStr, drop n2 xStr]
+  | otherwise = [x * 2024]
+  where
+    xStr = show x
+    nDigits = length xStr
+    n2 = nDigits `div` 2
+
+pInput :: Parser Stones
+pInput = decimal `sepBy1'` skipHorizontalSpace
 
 main :: IO ()
 main = do
-  d <- parseChallengeT (Sample 11 1) pInput
-  print d
+  d <- parseChallengeT (Full 11) pInput
+  print $ length $ nTimesStrict 25 (concatMap blink) d
