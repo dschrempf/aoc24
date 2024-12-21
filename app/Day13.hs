567ad6dffff9fefcd8a67e921c603560e6b767de
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-12-21 22:25:40 +0100

    day 13: parsing

--- app/Day13.hs
+++ app/Day13.hs
@@ -1,13 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <$>" #-}
+
 module Main
   ( main,
   )
 where
 
 import Aoc
-import Data.Attoparsec.Text (Parser)
+import Aoc.Parse (skipHorizontalSpace)
+import Data.Attoparsec.Text (Parser, decimal, parseOnly, sepBy1', skipSpace, string)
+import Data.Massiv.Array (Ix2 (..))
+import qualified Data.Text as T
 
-pInput :: Parser Text
-pInput = undefined
+data Configuration = Configuration
+  { buttonA :: !Ix2,
+    buttonB :: !Ix2,
+    prize :: !Ix2
+  }
+  deriving (Eq, Show)
+
+-- >>> parseOnly (pButtonConfiguration 'A') "Button A: X+94, Y+34"
+-- Right (94 :. 34)
+
+pButtonConfiguration :: Char -> Parser Ix2
+pButtonConfiguration c = do
+  _ <- string "Button"
+  _ <- skipHorizontalSpace
+  _ <- string $ T.singleton c <> ":"
+  _ <- skipHorizontalSpace
+  _ <- string "X+"
+  i <- decimal
+  _ <- string ", Y+"
+  j <- decimal
+  pure $ i :. j
+
+-- >>> parseOnly pPrize "Prize: X=8400, Y=5400"
+-- Right (8400 :. 5400)
+
+pPrize :: Parser Ix2
+pPrize = do
+  _ <- string "Prize: X="
+  i <- decimal
+  _ <- string ", Y="
+  j <- decimal
+  pure $ i :. j
+
+pConfiguration :: Parser Configuration
+pConfiguration = do
+  a <- pButtonConfiguration 'A'
+  _ <- skipSpace
+  b <- pButtonConfiguration 'B'
+  _ <- skipSpace
+  p <- pPrize
+  pure $ Configuration a b p
+
+pInput :: Parser [Configuration]
+pInput = pConfiguration `sepBy1'` skipSpace
 
 main :: IO ()
 main = do
