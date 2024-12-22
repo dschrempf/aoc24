7eb6322fe45eb7a8f4477c34164319f340586d99
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-12-22 13:54:07 +0100

    day 14: parser

--- app/Day14.hs
+++ app/Day14.hs
@@ -1,15 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main
   ( main,
   )
 where
 
 import Aoc
-import Data.Attoparsec.Text (Parser)
+import Data.Attoparsec.Text (Parser, char, decimal, sepBy1', signed, skipSpace, string)
+import Data.Massiv.Array (Ix2 (..))
 
-pInput :: Parser Text
-pInput = undefined
+data S = S {_p :: !Ix2, _q :: !Ix2}
+  deriving (Eq, Show)
+
+pIx2With :: Parser Int -> Parser Int -> Parser Ix2
+pIx2With pX pY = do
+  x <- pX
+  _ <- char ','
+  y <- pY
+  pure $ x :. y
+
+pS :: Parser S
+pS = do
+  _ <- string "p="
+  p <- pIx2With decimal decimal
+  _ <- string " v="
+  q <- pIx2With (signed decimal) (signed decimal)
+  pure $ S p q
+
+pInput :: Parser [S]
+pInput = pS `sepBy1'` skipSpace
 
 main :: IO ()
 main = do
   d <- parseChallengeT (Sample 14 1) pInput
-  print d
+  mapM_ print d
